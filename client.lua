a214a344ccb2820eeb35356f3005f9c01239fd58
JoeStarMama <ddashafieiadjbish2@glow.sch.uk>
2021-09-20 20:30:18 +0100

    Update client.lua

--- client.lua
+++ client.lua
@@ -1,18 +1,21 @@
+local minimap = RequestScaleformMovie("minimap")
+
+
 Citizen.CreateThread(function()
     while true do
         Wait(2000)
         if IsPedInAnyVehicle(PlayerPedId(-1), false) then
-            DisplayRadar(true)
+            ToggleRadar(true)
             SendNUIMessage({mapoutline = true})
+            TriggerVehicleLoop()
         else
-            DisplayRadar(false)
+            ToggleRadar(false)
             SendNUIMessage({mapoutline = false})
+            SetRadarZoom(1150)
         end
 
     end
 end)
- 
-SetRadarZoom(1150)
 
 local x = -0.025
 local y = -0.015
@@ -21,29 +24,33 @@ local h = 0.25
 
 Citizen.CreateThread(function()
 
-    local minimap = RequestScaleformMovie("minimap")
-    RequestStreamedTextureDict("circlemap", false)
-    while not HasStreamedTextureDictLoaded("circlemap") do Wait(100) end
-    AddReplaceTexture("platform:/textures/graphics", "radarmasksm", "circlemap",
-                      "radarmasksm")
+	RequestStreamedTextureDict("circlemap", false)
+	while not HasStreamedTextureDictLoaded("circlemap") do
+		Wait(100)
+	end
+
+	AddReplaceTexture("platform:/textures/graphics", "radarmasksm", "circlemap", "radarmasksm")
 
     SetMinimapClipType(1)
-    SetMinimapComponentPosition('minimap', 'L', 'B', x, y, w, h)
-    SetMinimapComponentPosition('minimap_mask', 'L', 'B', x + 0.17, y + 0.09,
-                                0.072, 0.162)
-    SetMinimapComponentPosition('minimap_blur', 'L', 'B', -0.035, -0.03, 0.18,
-                                0.22)
-    Wait(5000)
+    SetMinimapComponentPosition('minimap', 'L', 'B', -0.015, -0.025, 0.16, 0.24)
+    SetMinimapComponentPosition('minimap_mask', 'L', 'B', x + 0.17, y + 0.09, 0.072, 0.164)
+    SetMinimapComponentPosition('minimap_blur', 'L', 'B', -0.035, -0.03, 0.18, 0.22)
+    ThefeedSpsExtendWidescreenOn()
     SetRadarBigmapEnabled(true, false)
-    Wait(0)
+    Wait(150)
     SetRadarBigmapEnabled(false, false)
-
-    while true do
-        Wait(0)
-        BeginScaleformMovieMethod(minimap, "SETUP_HEALTH_ARMOUR")
-        ScaleformMovieMethodAddParamInt(3)
-        EndScaleformMovieMethod()
-        BeginScaleformMovieMethod(minimap, 'HIDE_SATNAV')
-        EndScaleformMovieMethod()
-    end
 end)
+
+ToggleRadar = function(state)
+	DisplayRadar(state)
+	BeginScaleformMovieMethod(minimap, "SETUP_HEALTH_ARMOUR")
+	ScaleformMovieMethodAddParamInt(3)
+	EndScaleformMovieMethod()
+end
+
+TriggerVehicleLoop = function()
+	Citizen.CreateThread(function()
+		ToggleRadar(true)
+        SetRadarBigmapEnabled(false, false)
+	end)
+end
